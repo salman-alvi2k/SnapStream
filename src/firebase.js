0936721c168568947169c61eeb37fc087db1f7b1
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-07-20 12:44:51 +0500

    fix: update firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore/lite'
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCl34zRCILUZxaR0zUIm8YVmrpE5vX5Ao8",
-    authDomain: "insta-clone-16478.firebaseapp.com",
-    databaseURL: "https://insta-clone-16478-default-rtdb.firebaseio.com",
-    projectId: "insta-clone-16478",
-    storageBucket: "insta-clone-16478.appspot.com",
-    messagingSenderId: "369023830323",
-    appId: "1:369023830323:web:5ae26382ec8d72ee24d646"
-  };
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const storage = getStorage(app);
 
 // onAuthStateChange(auth, user => {
